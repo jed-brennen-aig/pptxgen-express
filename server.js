5ef21ac0ba684fa71eb61f2b62e7a1f711174cf6
Jed Brennen <jed.brennen@aig.com>
2021-06-29 14:11:14 +0100

    Add some charts

--- server.js
+++ server.js
@@ -76,6 +76,58 @@ const generatePowerpoint = (data) => {
         fontFace: "Source Sans Pro",
         autoPage: true,
       });
+
+    let dataChartAreaLine = [
+      {
+        name: "Actual Sales",
+        labels: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec",
+        ],
+        values: [
+          1500, 4600, 5156, 3167, 8510, 8009, 6006, 7855, 12102, 12789, 10123,
+          15121,
+        ],
+      },
+      {
+        name: "Projected Sales",
+        labels: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec",
+        ],
+        values: [
+          1000, 2600, 3456, 4567, 5010, 6009, 7006, 8855, 9102, 10789, 11123,
+          12121,
+        ],
+      },
+    ];
+
+    pptx.addSlide().addChart(pptx.ChartType.line, dataChartAreaLine, {
+      x: 1,
+      y: 1,
+      w: 8,
+      h: 4,
+    });
   }
 
   return pptx.stream();
